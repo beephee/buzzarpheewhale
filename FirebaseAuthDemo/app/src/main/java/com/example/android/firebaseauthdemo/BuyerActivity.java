86874240fac1f307ff838ad8191d599be24c80f8
Wai Hung <ngwh@me.com>
2017-05-25 21:31:03 +0800

    Buyer dashboard refinement, added blank request submission screen

--- FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/BuyerActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/BuyerActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.firebaseauthdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuyerActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class BuyerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer);
+    }
+
+    //Goto Courier Dashboard
+    public void viewAddRequest(View view)
+    {
+        Intent intent = new Intent(this, AddRequestActivity.class);
+        startActivity(intent);
     }
 }
