edc75763da1a977bf4d3b44410ee916cc8ae246d
Wai Hung <ngwh@me.com>
2017-06-04 12:18:07 +0800

    Added preliminary chat skeleton

--- FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/MessagingFragment.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/MessagingFragment.java
@@ -1,29 +1,125 @@
 package com.example.android.firebaseauthdemo;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static com.example.android.firebaseauthdemo.R.id.listViewProducts;
 
 
 public class MessagingFragment extends Fragment {
+
+    private Button add_room;
+    private EditText room_name;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private String name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    String userEmail;
 
     public static MessagingFragment newInstance() {
         MessagingFragment fragment = new MessagingFragment();
         return fragment;
     }
 
-    String userEmail;
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        add_room = (Button) getView().findViewById(R.id.btn_add_room);
+        room_name = (EditText) getView().findViewById(R.id.room_name_edittext);
+        listView = (ListView) getView().findViewById(R.id.listView);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_messaging, container, false);
 
         Bundle extras = getActivity().getIntent().getExtras();
-        if(extras != null){
+        if (extras != null) {
             userEmail = extras.getString("email");
         }
         return rootView;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_of_rooms);
+        listView.setAdapter(arrayAdapter);
+
+        name = userEmail.split("@", 2)[0];
+
+        add_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put(room_name.getText().toString(), "");
+                root.child("chat").updateChildren(map);
+
+            }
+        });
+
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.child("chat").getChildren().iterator();
+
+                while (i.hasNext()) {
+                    set.add(((DataSnapshot) i.next()).getKey());
+                }
+
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getActivity().getApplicationContext(), ChatRoomActivity.class);
+                intent.putExtra("room_name", ((TextView) view).getText().toString());
+                intent.putExtra("user_name", name);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
+
+
