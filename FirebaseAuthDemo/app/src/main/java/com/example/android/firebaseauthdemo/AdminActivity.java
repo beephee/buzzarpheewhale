c56a74927e089be6268107b9774b1f3c058189a4
beephee <draciel_g@hotmail.com>
2017-06-10 18:51:06 +0800

    Added ability to ban/unban user and give/remove admin rights

--- FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/AdminActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/AdminActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.firebaseauthdemo;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,6 +20,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.sql.Types.BOOLEAN;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -34,11 +42,91 @@ public class AdminActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 User user = userList.get(i);
-                //To add dialog with Ban/Unban/Set Admin
+                showMenuDialog(user.getuserUID(),user.getuserEmail(),user.getuserType(),user.getBlacklisted());
                 return true;
             }
         });
 
+    }
+
+    private void showMenuDialog(final String userUID, final String userEmail, final String userType, final String blacklisted) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.admin_menu, null);
+        dialogBuilder.setView(dialogView);
+        final Button banUserButton = (Button) dialogView.findViewById(R.id.banButton);
+        final Button unbanUserButton = (Button) dialogView.findViewById(R.id.unbanButton);
+        final Button setAdminUserButton = (Button) dialogView.findViewById(R.id.setAdminButton);
+        final Button unadminUserButton = (Button) dialogView.findViewById(R.id.unadminButton);
+
+        final AlertDialog b = dialogBuilder.create();
+        b.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        b.show();
+
+        banUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                banUser(userUID, userEmail, userType, blacklisted);
+                b.dismiss();
+            }
+        });
+
+        unbanUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unbanUser(userUID, userEmail, userType, blacklisted);
+                b.dismiss();
+            }
+        });
+
+        setAdminUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAdminUser(userUID, userEmail, userType, blacklisted);
+                b.dismiss();
+            }
+        });
+
+        unadminUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unadminUser(userUID, userEmail, userType, blacklisted);
+                b.dismiss();
+            }
+        });
+    }
+
+    private boolean banUser(String userUID, String userEmail, String userType, String blacklisted) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(userUID);
+        User user = new User(userUID, userEmail, userType, "true");
+        dR.setValue(user);
+        Toast.makeText(getApplicationContext(), "User banned!", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean unbanUser(String userUID, String userEmail, String userType, String blacklisted) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(userUID);
+        User user = new User(userUID, userEmail, userType, "false");
+        dR.setValue(user);
+        Toast.makeText(getApplicationContext(), "User unbanned!", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean setAdminUser(String userUID, String userEmail, String userType, String blacklisted) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(userUID);
+        User user = new User(userUID, userEmail, "admin", blacklisted);
+        dR.setValue(user);
+        Toast.makeText(getApplicationContext(), "User given admin status!", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean unadminUser(String userUID, String userEmail, String userType, String blacklisted) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(userUID);
+        User user = new User(userUID, userEmail, "registered", blacklisted);
+        dR.setValue(user);
+        Toast.makeText(getApplicationContext(), "Admin status removed!", Toast.LENGTH_LONG).show();
+        return true;
     }
 
     @Override
--- FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/UserList.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/UserList.java
@@ -2,12 +2,15 @@ package com.example.android.firebaseauthdemo;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -50,5 +53,6 @@ public class UserList extends ArrayAdapter<User>{
 
         return listViewUsers;
     }
+
 
 }
