f9bd3a49c153106add1ff8715e79c815064f1146
Wai Hung <ngwh@me.com>
2017-06-12 18:08:30 +0800

    Changed courier fragment to function with filter buttons

--- FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/AcceptedCourierFragment.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/android/firebaseauthdemo/AcceptedCourierFragment.java
@@ -1,8 +1,8 @@
 package com.example.android.firebaseauthdemo;
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -15,17 +15,16 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.ittianyu.bottomnavigationviewex.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.example.android.firebaseauthdemo.R.id.listViewProducts;
 
 public class AcceptedCourierFragment extends Fragment {
 
@@ -34,21 +33,45 @@ public class AcceptedCourierFragment extends Fragment {
         return fragment;
     }
 
-    DatabaseReference databaseProducts;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    DatabaseReference databaseUsers;
+    DatabaseReference databaseProductsAccepted;
+    DatabaseReference databaseProductsAll;
     ListView listViewProductsAccepted;
-    List<Product> productList;
+    List<Product> productListAccepted;
+    ListView listViewProductsAll;
+    List<Product> productListAll;
+    ProductList adapterAll;
     String userEmail;
-    private BottomNavigationViewEx bottomNavigationViewCourier;
+    String listFilter;
+    Button btnAccepted;
+    Button btnSuggested;
+    Button btnAll;
     private Button suggestedOrders;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_courier_accepted, container, false);
+        //To remove once filter tabs are finalized
         Button suggestedOrders = (Button) rootView.findViewById(R.id.suggestedButton);
         suggestedOrders.setOnClickListener(suggestedListener);
 
-        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
-        productList = new ArrayList<>();
+        databaseProductsAccepted = FirebaseDatabase.getInstance().getReference("products");
+        databaseProductsAll = FirebaseDatabase.getInstance().getReference("products");
+        productListAccepted = new ArrayList<>();
+        productListAll = new ArrayList<>();
+        listFilter = "suggested";
+
+        //Filter Buttons
+        btnAccepted = (Button) rootView.findViewById(R.id.btnAccepted);
+        btnSuggested = (Button) rootView.findViewById(R.id.btnSuggested);
+        btnAll = (Button) rootView.findViewById(R.id.btnAll);
+        btnAccepted.setOnClickListener(btnAcceptedListener);
+        btnSuggested.setOnClickListener(btnSuggestedListener);
+        btnAll.setOnClickListener(btnAllListener);
+        btnAccepted.setTypeface(Typeface.DEFAULT_BOLD);
+        btnAccepted.setTextSize(16);
 
         //Grabs email string from previous activity
         Bundle extras = getActivity().getIntent().getExtras();
@@ -58,20 +81,86 @@ public class AcceptedCourierFragment extends Fragment {
         return rootView;
     }
 
+    //Accepted Filter Button
+    private View.OnClickListener btnAcceptedListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            listViewProductsAccepted.setVisibility(View.VISIBLE);
+            listViewProductsAll.setVisibility(View.INVISIBLE);
+            clearButtonStyle();
+            btnAccepted.setTypeface(Typeface.DEFAULT_BOLD);
+            btnAccepted.setTextSize(16);
+        }
+    };
+
+    //Suggested Filter Button (Under Construction)
+    private View.OnClickListener btnSuggestedListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Toast.makeText(getActivity().getApplicationContext(), "Under construction", Toast.LENGTH_LONG).show();
+            listFilter = "suggested";
+            listViewProductsAccepted.setVisibility(View.INVISIBLE);
+            listViewProductsAll.setVisibility(View.VISIBLE);
+            onStart();
+            adapterAll.notifyDataSetChanged();
+            clearButtonStyle();
+            btnSuggested.setTypeface(Typeface.DEFAULT_BOLD);
+            btnSuggested.setTextSize(16);
+        }
+    };
+
+    //Display All Button
+    private View.OnClickListener btnAllListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            listFilter = "all";
+            listViewProductsAccepted.setVisibility(View.INVISIBLE);
+            listViewProductsAll.setVisibility(View.VISIBLE);
+            onStart();
+            adapterAll.notifyDataSetChanged();
+            clearButtonStyle();
+            btnAll.setTypeface(Typeface.DEFAULT_BOLD);
+            btnAll.setTextSize(16);
+        }
+    };
+
+    //Reset Button Styles
+    private void clearButtonStyle(){
+        Button btnAccepted = (Button) getView().findViewById(R.id.btnAccepted);
+        btnAccepted.setTypeface(Typeface.SANS_SERIF);
+        btnAccepted.setTextSize(14);
+        Button btnSuggested = (Button) getView().findViewById(R.id.btnSuggested);
+        btnSuggested.setTypeface(Typeface.SANS_SERIF);
+        btnSuggested.setTextSize(14);
+        Button btnAll = (Button) getView().findViewById(R.id.btnAll);
+        btnAll.setTypeface(Typeface.SANS_SERIF);
+        btnAll.setTextSize(14);
+    }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         listViewProductsAccepted = (ListView) getView().findViewById(R.id.listViewProductsAccepted);
+        listViewProductsAll = (ListView) getView().findViewById(R.id.listViewProductsAll);
 
         listViewProductsAccepted.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Product product = productList.get(i);
+                Product product = productListAccepted.get(i);
                 showMenuDialog(product.getProductId(), product.getProductBuyer(), product.getProductCourier(), product.getProductName(), product.getProductType(), product.getProductCoords(), product.getLength(), product.getWidth(), product.getHeight(), product.getWeight(), product.getPrice(), product.getDate(), product.getImgurl(), product.getCountry(), product.getCourierComplete(), product.getBuyerComplete(), product.getTransit(), product.getBuyerPaid());
+                return true;
+            }
+        });
+        listViewProductsAll.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Product product = productListAll.get(i);
+                String productID = product.getProductId();
+                DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(productID);
+                dR.child("productCourier").setValue(userEmail);
+                Toast.makeText(getActivity().getApplicationContext(), "Order accepted!", Toast.LENGTH_LONG).show();
                 return true;
             }
         });
     }
 
+    //Menu Dialog for Accepted Tab
     private void showMenuDialog(final String productId, final String productBuyer, final String productCourier, final String productName, final String productType, final String productCoords, final String length, final String width, final String height, final String weight, final String price, final String date, final String url, final String country, final Boolean courierAccept, final Boolean buyerAccept, final Boolean transit, final Boolean buyerPaid) {
 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
@@ -126,7 +215,7 @@ public class AcceptedCourierFragment extends Fragment {
         Toast.makeText(getActivity().getApplicationContext(), "Order in transit!", Toast.LENGTH_LONG).show();
         return true;
     }
-
+    
     private boolean updateComplete(String productId, String productBuyer, String productCourier, String productName, String productType, String productCoords, String length, String width, String height, String weight, String price, String date, String url, String country, Boolean courierAccept, Boolean buyerAccept, Boolean transit, Boolean buyerPaid) {
         DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(productId);
         Product product = new Product(productId, productBuyer, productCourier, productName, productType, productCoords, length, width, height, weight, price, date, url, country, true, buyerAccept, transit, buyerPaid);
@@ -135,6 +224,7 @@ public class AcceptedCourierFragment extends Fragment {
         return true;
     }
 
+    //Can remove once filter tabs are finalized
     private View.OnClickListener suggestedListener = new View.OnClickListener() {
         public void onClick(View v) {
             // Create new fragment and transaction
@@ -157,24 +247,72 @@ public class AcceptedCourierFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        databaseProducts.addValueEventListener(new ValueEventListener() {
+        //Instantiate user to retrieve country information
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //Accepted Tab
+        databaseProductsAccepted.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                productList.clear();
+                productListAccepted.clear();
 
                 for(DataSnapshot productSnapshot : dataSnapshot.getChildren()){
                     Product product = productSnapshot.getValue(Product.class);
                     // Only include items that are accepted by courier
                     String courierID = product.getProductCourier();
                     if (courierID.equals(userEmail)) {
-                        productList.add(product);
+                        productListAccepted.add(product);
                     }
                 }
 
-                ProductListAccepted adapter = new ProductListAccepted(getActivity(), productList);
+                ProductListAccepted adapter = new ProductListAccepted(getActivity(), productListAccepted);
                 listViewProductsAccepted.setAdapter(adapter);
 
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //Suggested & Display All Tab
+        databaseProductsAll.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                productListAll.clear();
+
+                for(DataSnapshot productSnapshot : dataSnapshot.getChildren()){
+                    Product product = productSnapshot.getValue(Product.class);
+
+                    String courierID = product.getProductCourier();
+                    String buyerID = product.getProductBuyer();
+                    /* To add after user settings are implemented */
+                    //String productCountry = product.getCountry();
+                    //String userCountry = user.getCountry();
+
+                    // Only include items that are not assigned to any courier yet and not requested by user
+                    if (courierID.equals("NONE") && !buyerID.equals(userEmail)) {
+                        switch (listFilter) {
+                            //Only add requests in same country as user
+                            case "suggested":
+                                /*if(user.getCountry.equals(product.getCountry())){
+                                    productListAll.add(product);
+                                }*/
+                                break;
+                            //List all requests
+                            case "all":
+                                productListAll.add(product);
+                                break;
+                        }
+                    }
+                }
+                adapterAll = new ProductList(getActivity(), productListAll);
+                listViewProductsAll.setAdapter(adapterAll);
             }
 
             @Override
